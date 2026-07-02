f2feeefa829f8a130ac7af81330d0f7d2835160e
Mustafa Muhaimin <mumufahad@gmail.com>
2026-07-02 14:44:58 +0600

    debug log

--- core/settings.py
+++ core/settings.py
@@ -229,3 +229,16 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+LOGGING = {
+    "version": 1,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
